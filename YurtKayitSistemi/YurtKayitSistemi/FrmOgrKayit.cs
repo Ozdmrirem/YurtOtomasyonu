9220469c9a54e5bcf4de3bbe4699aed2708bd42b
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-11-06 22:29:54 +0300

    Add files via upload

---
+++ YurtKayitSistemi/YurtKayitSistemi/FrmOgrKayit.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace YurtKayitSistemi
+{
+    public partial class FrmOgrKayit : Form
+    {
+        public FrmOgrKayit()
+        {
+            InitializeComponent();
+        }
+
+        private void txtOgrSoyad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-HJ161GJ\SQLEXPRESS; Initial Catalog = YurtKayit; Integrated Security = True; Encrypt=False");
+
+       
+        private void FrmOgrKayit_Load(object sender, EventArgs e)
+        {
+            //Bölümleri Listeleme Komutları
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select BolumAd From Bolumler", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                CmbBolum.Items.Add(oku[0].ToString());
+            }
+            baglanti.Close();
+
+            //Boş Odaları Listeleme Komutları
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select OdaNo From Odalar where OdaKapasite != OdaAktif", baglanti);
+            SqlDataReader oku2 = komut2.ExecuteReader();
+            while (oku2.Read())
+            {
+                CmbOdaNo.Items.Add(oku2[0].ToString());
+            }
+            baglanti.Close();
+        }
+    }
+}
+//Data Source = DESKTOP - HJ161GJ\SQLEXPRESS; Initial Catalog = YurtKayit; Integrated Security = True; Encrypt = False; Trust Server Certificate=True
