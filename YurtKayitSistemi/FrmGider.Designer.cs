00595f0bb692c918ad6b6f66fd502de1932538f8
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-11-26 22:42:30 +0300

    Add files via upload

---
+++ YurtKayitSistemi/FrmGider.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace YurtKayitSistemi
+{
+    partial class FrmGider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGider));
+            this.txtElektrik = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDogalgaz = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInternet = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGida = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPersonel = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDiger = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtElektrik
+            // 
+            this.txtElektrik.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtElektrik.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtElektrik.Location = new System.Drawing.Point(156, 31);
+            this.txtElektrik.Margin = new System.Windows.Forms.Padding(4);
+            this.txtElektrik.Name = "txtElektrik";
+            this.txtElektrik.Size = new System.Drawing.Size(374, 30);
+            this.txtElektrik.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(44, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Elektrik :";
+            // 
+            // txtSu
+            // 
+            this.txtSu.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtSu.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSu.Location = new System.Drawing.Point(156, 69);
+            this.txtSu.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSu.Name = "txtSu";
+            this.txtSu.Size = new System.Drawing.Size(374, 30);
+            this.txtSu.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(87, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Su :";
+            // 
+            // txtDogalgaz
+            // 
+            this.txtDogalgaz.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtDogalgaz.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtDogalgaz.Location = new System.Drawing.Point(156, 107);
+            this.txtDogalgaz.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDogalgaz.Name = "txtDogalgaz";
+            this.txtDogalgaz.Size = new System.Drawing.Size(374, 30);
+            this.txtDogalgaz.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(36, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Doğalgaz :";
+            // 
+            // txtInternet
+            // 
+            this.txtInternet.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtInternet.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtInternet.Location = new System.Drawing.Point(156, 145);
+            this.txtInternet.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInternet.Name = "txtInternet";
+            this.txtInternet.Size = new System.Drawing.Size(374, 30);
+            this.txtInternet.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(42, 150);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "İnternet :";
+            // 
+            // txtGida
+            // 
+            this.txtGida.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtGida.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGida.Location = new System.Drawing.Point(156, 183);
+            this.txtGida.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGida.Name = "txtGida";
+            this.txtGida.Size = new System.Drawing.Size(374, 30);
+            this.txtGida.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(71, 188);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Gıda :";
+            // 
+            // txtPersonel
+            // 
+            this.txtPersonel.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtPersonel.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPersonel.Location = new System.Drawing.Point(156, 221);
+            this.txtPersonel.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPersonel.Name = "txtPersonel";
+            this.txtPersonel.Size = new System.Drawing.Size(374, 30);
+            this.txtPersonel.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(38, 226);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Personel :";
+            // 
+            // txtDiger
+            // 
+            this.txtDiger.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtDiger.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtDiger.Location = new System.Drawing.Point(156, 259);
+            this.txtDiger.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDiger.Name = "txtDiger";
+            this.txtDiger.Size = new System.Drawing.Size(374, 30);
+            this.txtDiger.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(65, 264);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 21);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Diğer :";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(260, 315);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(167, 33);
+            this.btnKaydet.TabIndex = 16;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // FrmGider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(633, 406);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtDiger);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtPersonel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtGida);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtInternet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDogalgaz);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtElektrik);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmGider";
+            this.Text = "Giderler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElektrik;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDogalgaz;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInternet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGida;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPersonel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDiger;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
